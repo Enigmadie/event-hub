1c36b34b6a85f2c1e25fe764dc97a3b0630db550
Enigmadie <gagen666@mail.ru>
2026-02-05 00:02:30 +0300

    init transport layer

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,39 @@
+use event_hub::transport::mqqt::client::{MqttConfig, MqttRuntime};
+use rumqttc::{Event, Packet, QoS};
+
 fn main() {
-    println!("Hello, world!");
+    env_logger::init();
+
+    let mut mqtt = MqttRuntime::connect(MqttConfig {
+        client_id: "home".to_string(),
+        host: "192.168.0.219".to_string(),
+        port: 1883,
+    });
+
+    mqtt.client
+        .subscribe("zigbee2mqtt/+/state", QoS::AtLeastOnce);
+
+    mqtt.client
+        .subscribe("zigbee2mqtt/+/availability", QoS::AtLeastOnce);
+
+    mqtt.client
+        .subscribe("zigbee2mqtt/+/power", QoS::AtLeastOnce);
+
+    println!("Home is listening...");
+
+    for event in mqtt.connection.iter() {
+        match event {
+            Ok(Event::Incoming(Packet::Publish(p))) => {
+                let topic = p.topic;
+                let payload = String::from_utf8_lossy(&p.payload);
+
+                println!("{} → {}", topic, payload);
+            }
+            Ok(_) => {}
+            Err(e) => {
+                eprintln!("MQTT error: {:?}", e);
+                break;
+            }
+        }
+    }
 }
---
+++ src/transport/mqqt/client.rs
@@ -0,0 +1,26 @@
+use std::time::Duration;
+
+use rumqttc::{Client, MqttOptions};
+
+pub struct MqttConfig {
+    pub client_id: String,
+    pub host: String,
+    pub port: u16,
+}
+
+pub struct MqttRuntime {
+    pub client: rumqttc::Client,
+    pub connection: rumqttc::Connection,
+}
+
+impl MqttRuntime {
+    pub fn connect(config: MqttConfig) -> Self {
+        let mut mqttoptions = MqttOptions::new(config.client_id, config.host, config.port);
+        mqttoptions.set_keep_alive(Duration::from_secs(30));
+        mqttoptions.set_max_packet_size(1024 * 1024, 1024 * 1024);
+
+        let (client, connection) = Client::new(mqttoptions, 10);
+
+        Self { client, connection }
+    }
+}
